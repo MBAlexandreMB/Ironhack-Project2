9e5e919580e57b5bf031e3912bb1d1d002483ac3
Renan Soares <renan.soares.francisco@gmail.com>
2019-07-13 21:13:40 -0300

    new routes - login/cv

--- routes/user.js
+++ routes/user.js
@@ -1,48 +1,63 @@
+//NPM MODULES
 const express = require('express');
 const router = express.Router();
-const passport = require("passport");
-const User = require('../models/user')
-
-const bcrypt = require("bcrypt");
+const passport = require('passport');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
+//---------------------------------------------------------------------------
 
+// SIGNUP ROUTER
+// CLIENT REQUISITION
 router.get('/signup', (req, res, next) => {
   res.render('user/signup');
 });
 
-router.post('/signup', (req, res, next) => {
-  const {username, email, password} = req.body
-
-  User.findOne({ username: username })
-  .then(user => {
-    if (user !== null) {
-      res.render("user/signup", { message: "The username already exists" });
-      return;
-    }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username: username,
-      password: hashPass,
-      email: email,
-
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        res.render("user/signup", { message: "Something went wrong" });
-      } else {
-        res.redirect("/");
-      }
-    });
-  })
-  .catch(error => {
-    next(error)
-  })
+router.get('/cv', (req, res, next) => {
+  res.render('user/cv');
 });
 
+router.post('/signup', (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  User.findOne({"user.username": username})
+    .then((user) => { console.log(user)
+      if (user) {
+        res.render('user/signup', { message: 'The username already exists' });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        user: {
+          username: username,
+          password: hashPass
+        },
+        personal: {
+          email: email
+        }
+      });
+
+      newUser.save((err) => {
+        if (err) {
+          res.render('user/signup', { message: 'Something went wrong' });
+        } else {
+          res.redirect('/user/cv');
+        }
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+
+//---------------------------------------------------------------------------
+
+// SIGNUP ROUTER
+// CLIENT REQUISITION
 router.get('/login', (req, res, next) => {
   res.render('user/login');
 });
@@ -50,5 +65,7 @@ router.get('/login', (req, res, next) => {
 router.get('/logout', (req, res, next) => {
   res.render('user/logout');
 });
+
+//---------------------------------------------------------------------------
 
 module.exports = router;
