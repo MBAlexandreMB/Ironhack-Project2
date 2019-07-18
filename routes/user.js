b26a5cec10236035797b83c45e532db3014238ae
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2019-07-18 14:50:18 -0300

    User performance fully working

--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,6 @@ const Process = require('../models/process');
 //LOGIN VERIFICATION
 function ensureUserLoggedIn() {
   return function(req, res, next) {
-    console.log(req);
     if (req.isAuthenticated() && !req.user.ein) {
       return next();
     } else {
@@ -307,7 +306,7 @@ router.post(
         });
         //---------------------------------------------------------------------------
         
-
+        
         // // PROFILE ROUTER
         // // router.get('/profile/:userID', ensureUserLoggedIn(), (req, res, next) => {
         // //   res.render(`user/profile`, req.user);
@@ -316,7 +315,7 @@ router.post(
         // // router.get('/profile/', ensureUserLoggedIn(), (req, res, next) => {
         // //   res.redirect(`/user/profile/${req.user._id}`);
         // // });
-
+        
         //---------------------------------------------------------------------------
         
         router.get('/logout', (req, res) => {
@@ -329,7 +328,7 @@ router.post(
         router.get('/confirmation/company/:companyID/process/:processID', ensureUserLoggedIn('/user/login'), (req, res, next) => {
           const getCompany = Company.findById(req.params.companyID);
           const getProcess = Process.findById(req.params.processID);
-
+          
           Promise.all([getCompany, getProcess])
           .then(result => {
             User.findOneAndUpdate(
@@ -339,11 +338,11 @@ router.post(
             )
             .then((user) => {
               if (user) {
-                res.render('user/confirmationCode', {
+                res.render('user/confirmationProcess', {
                   message: `You are enrolled in "${result[0].name} - ${result[1].title}" process!`
                 });
               } else {
-                res.render('user/confirmationCode', {
+                res.render('user/confirmationProcess', {
                   message: "We didn't find your account. Are you sure you're logged in?"
                 });
               }
@@ -352,7 +351,7 @@ router.post(
           })
           .catch(err => {
             console.log(err);
-            res.render('user/confirmationCode', {
+            res.render('user/confirmationProcess', {
               message: "We didn't find any process within this adress!"
             });
           });
@@ -377,7 +376,7 @@ router.post(
           res.redirect(`/user/profile/${req.user._id}`);
         });
         //---------------------------------------------------------------------------
-
+        
         
         // PROCESS LIST
         router.get('/profile/:userID/processes', ensureLoggedIn('/'), (req, res, next) => {
@@ -388,73 +387,64 @@ router.post(
         //---------------------------------------------------------------------------
         
         
-        module.exports = router;
- 
+        // PERFORMANCE LIST
+        router.get('/profile/:userID/performance', ensureLoggedIn('/'), (req, res, next) => {
+          console.log('----------------------------------')
+          const performance = {};
+          const questCatRel = {};
+          User.findById(req.params.userID)
+          .then(user => {
 
+            let answeredQuestions = new Set();
+            user.questions.forEach(item => {
+              answeredQuestions.add(item.question);
+            });
+            answeredQuestions = Array.from(answeredQuestions);
+            
+            Questions.find({_id: {$in: answeredQuestions}})
+            .then(resultQuestions => {
 
+              let answerCategories = new Set();
+              resultQuestions.forEach(item => {
+                answerCategories.add(item.category);
+                questCatRel[item._id] = item.category;
+              });
+              answerCategories = Array.from(answerCategories);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// PERFORMANCE LIST
-router.get('/profile/:userID/performance', ensureLoggedIn('/'), (req, res, next) => {
-  User.findById(req.params.userID).populate('questions')
-  .then(user => {
-  let totalQuestions = user.questions.length;
-  let correctAnswers = user.questions.reduce((total, question) => {
-    if(question.statusAnswer) {
-    return total += 1
-    }
-  });
-  let categoryArray = user.questions.map((element) => {
-    return element.question;
-  });
-  Questions.find({_id: {$in: categoryArray}})
-  .then(category => res.render('user/performance', category))
-  .catch(error => console.log(error))
-})
-  .catch(error => console.log(error))
-});
-//---------------------------------------------------------------------------
-
-
-
-
-
+              Category.find({_id: {$in: answerCategories}})
+              .then(categories => {
+                categories.forEach(category => {
+                  performance[category._id] = {
+                    name: category.name,
+                    correct: 0,
+                    total: 0,
+                    percentage: 0,
+                  };
+                });
+                
+                let multiplier = 0;
+                user.questions.forEach(item => {
+                  for(let x = 0; x < resultQuestions.length; x += 1) {
+                    if (String(resultQuestions[x]._id) === String(item.question)) {
+                      multiplier = resultQuestions[x].difficulty;
+                    }
+                  }
+                  if (item.statusAnswer) {
+                    performance[questCatRel[item.question]].correct += 1 * multiplier;
+                  }
+                  performance[questCatRel[item.question]].total += 1 * multiplier;
+                  performance[questCatRel[item.question]].percentage = ((performance[questCatRel[item.question]].correct /  performance[questCatRel[item.question]].total) * 100) + '%';
+                });
+                console.log(user);
+                res.render('user/performance', {performance, user});
+              })
+              .catch(err => console.log(err));        
+            })
+            .catch(err => console.log(err));            
+          })
+          .catch(err => console.log(err)); 
+        });
+        //---------------------------------------------------------------------------
         
         //CARD       
         function shuffle(array) {
@@ -476,7 +466,6 @@ router.get('/profile/:userID/performance', ensureLoggedIn('/'), (req, res, next)
         const getACard = (process, companyName, categories, difficulties, userAnswers) => {
           
           const promise = new Promise((resolve, reject) => {
-            console.log('-------------------------------------')
             let questionArr = [];
             let leftToAnswer = 0;
             let totalToAnswer = 0;
@@ -607,24 +596,29 @@ router.get('/profile/:userID/performance', ensureLoggedIn('/'), (req, res, next)
         });
         
         router.post('/process/test/saveAnswer', (req, res, next) => {
+          console.log('---------------------------');
           const {question, answer} = req.query;
-          Questions.findById(question, {correctAlternative: 1, _id: 0})
-          .then(result => {
-            const statusAnswer = String(result.correctAlternative) === String(answer) ? true : false;
-            User.findByIdAndUpdate(req.user._id, {$push: {questions: {
-              question,
-              answer,
-              statusAnswer,
-            }}})
-            .then(result => res.status(200).json(result))
-            .catch(err => res.status(401).json(err));
+          User.findOne({$and: [{_id: req.user._id},{'questions.question': question}]})
+          .then(user => {
+            if (!user) {
+              Questions.findById(question, {correctAlternative: 1, _id: 0})
+              .then(result => {
+                const statusAnswer = String(result.correctAlternative) === String(answer) ? true : false;
+                User.findByIdAndUpdate(req.user._id, {$push: {questions: {
+                  question,
+                  answer,
+                  statusAnswer,
+                }}})
+                .then(result => res.status(200).json(result))
+                .catch(err => res.status(401).json(err));
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+              });
+            }
           })
-          .catch(err => {
-            console.log(err);
-            res.status(400).json(err)
-          });
-          
+          .catch(err => console.log(err));          
         });
         
         module.exports = router;
-        
